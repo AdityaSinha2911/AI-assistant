a84e7ad8669fece5091f075410b9d3cf14a4301f
AdityaSinha2911 <adityahjp11111@gmail.com>
2026-02-11 18:12:23 +0530

    successfully commited

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+#this is just created to test
+
+music={
+    "jharokhe":"https://www.youtube.com/watch?v=KqpIIaCJggY&list=RDKqpIIaCJggY&start_radio=1",
+    "valam":"https://www.youtube.com/watch?v=tQsIFoAzzRs&list=RDKqpIIaCJggY&index=10"
+}
